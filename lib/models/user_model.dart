96cec739547c537e28c753712047351890ed38b1
mainhathuy <nhathuysg2000@gmail.com>
2026-04-19 11:36:56 +0700

    test

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,10 +2,13 @@ class UserModel {
   final String name;
   final String email;
   final String avatarUrl;
-
+  final String password;
+  final String role;
   UserModel({
     required this.name,
     required this.email,
+    required this.password,
+    required this.role,
     this.avatarUrl = "",
   });
 }
